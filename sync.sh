b0edb15a7f77ddfd87845d1c3eb6b99ea0b367de
Stefan Penner <stefan.penner@gmail.com>
2011-10-23 16:21:40 -0500

    stuff

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+backup=~/.dotfiles_backup/$(date +%m%d%H%M%Y%S)
+mkdir -p $backup
+for file in $(ls -a | grep '^\.\w')
+do
+  mv ~/$file $backup 2>/dev/null
+  echo " $file"
+  ln -s $(pwd)/$file ~
+done
