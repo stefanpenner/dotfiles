bd9f7728c20a352755421f98a46e85285d57ec7e
Stefan Penner <stefan.penner@gmail.com>
2016-12-02 22:32:23 -0800

    fixup the backup sync

--- sync.sh
+++ sync.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-backup=~/.dotfiles_backup/$(date +%m%d%H%M%Y%S)
+backup=~/.dotfiles_backup/$(date +%m%d%H%M%Y%S
+__dirname=$(dirname %0)
 mkdir -p $backup
 for file in $(ls -a | grep '^\.\w')
 do
   mv ~/$file $backup 2>/dev/null
-  echo " $file"
-  ln -s $(pwd)/$file ~
+  echo " $__dirname/$file"
+  ln -s $__dirname/$file ~
   rm -rf ~/.git
 done
