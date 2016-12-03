60af96939bd78bfcd775e70a70f3dd64991cfc8b
Stefan Penner <stefan.penner@gmail.com>
2016-12-02 23:19:25 -0800

    * change default shell * disable vim setup, as it likely needs a special vim script. +PlugInstall +qall! doesn’t happen in serial, rather concurrently.

--- setup.sh
+++ setup.sh
@@ -10,11 +10,10 @@ add-apt-repository ppa:neovim-ppa/unstable && \
 add-apt-repository ppa:saiarcot895/myppa && \
 apt-fast update && \
 apt-fast -qq install fish python-software-properties neovim jq htop git curl wget thefuck tmux  build-essential && \
-mkdir -p $HOME/src/stefanpenner && \
+chsh -s (which fish) && \
 git clone https://github.com/stefanpenner/dotfiles $HOME/src/stefanpenner/dotfiles && \
 $HOME/src/stefanpenner/dotfiles/sync.sh && \
-nvim +PlugInstall +qall! && \
-
+# nvim +PlugInstall +qall! && \ # qall! happens to soon, likely need a custom vim script...
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
 curl -sSL https://get.rvm.io | bash -s stable --ruby=latest
 
