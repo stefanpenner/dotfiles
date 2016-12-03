55fc76bcbf54a1e193ba171d73745d4df4f202d3
Stefan Penner <stefan.penner@gmail.com>
2016-12-02 22:09:29 -0800

    updates

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 # TODO:
 #  * switch for OSX
 #  * add CI
-apt-get update
-apt-get -qq install software-properties-common python-software-properties
-add-apt-repository ppa:neovim-ppa/unstable
-apt-get update
-apt-get -qq install fish python-software-properties neovim jq htop git curl wget thefuck tmux
-mkdir -p $HOME/src/stefanpenner
-git clone https://github.com/stefanpenner/dotfiles $HOME/src/stefanpenner/dotfiles
-$HOME/src/stefanpenner/dotfiles/sync.sh
-nvim +PlugInstall +qall!
+apt-get update && \
+apt-get -qq install software-properties-common python-software-properties && \
+add-apt-repository ppa:neovim-ppa/unstable && \
+apt-get update && \
+apt-get -qq install fish python-software-properties neovim jq htop git curl wget thefuck tmux  build-essential && \
+mkdir -p $HOME/src/stefanpenner && \
+git clone https://github.com/stefanpenner/dotfiles $HOME/src/stefanpenner/dotfiles && \
+$HOME/src/stefanpenner/dotfiles/sync.sh && \
+nvim +PlugInstall +qall! && \
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
 curl -sSL https://get.rvm.io | bash -s stable --ruby=latest
+
+
