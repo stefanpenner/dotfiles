4e1c4c22f562b4da85960f24af561ad53f0b7bb9
Stefan Penner <stefan.penner@gmail.com>
2017-01-15 14:13:43 -0800

    make global var use more specific name

--- .config/node/main.fish
+++ .config/node/main.fish
@@ -1,12 +1,12 @@
-set root $HOME/.config/node
-set remote "https://nodejs.org/download/release"
+set FISH_NODE_ROOT $HOME/.config/node
+set FISH_NODE_REMOTE "https://nodejs.org/download/release"
 
 function node-setup
-  mkdir -p $root/{versions,default,tarballs,checksums,cache}
+  mkdir -p $FISH_NODE_ROOT/{versions,default,tarballs,checksums,cache}
 end
 
 function node-start-again
-  rm -rf $root/{versions,default,tarballs,checksums,cache}
+  rm -rf $FISH_NODE_ROOT/{versions,default,tarballs,checksums,cache}
   node-setup
 end
 
@@ -24,9 +24,9 @@ set _node_current_platform (_get_node_current_platform)
 function node-uninstall
   set -l _version $argv[1]
   set -l filename node-$_version-$_node_current_platform.tar.gz
-  set -l tarball $root/tarballs/$filename
-  set -l target $root/versions/node-$_version-$_get_node_current_platform/
-  set -l checksum "$root/checksums/$_version-SHASUMS256.txt"
+  set -l tarball $FISH_NODE_ROOT/tarballs/$filename
+  set -l target $FISH_NODE_ROOT/versions/node-$_version-$_node_current_platform/
+  set -l checksum "$FISH_NODE_ROOT/checksums/$_version-SHASUMS256.txt"
 
   rm -rf $tarball $target $checksum
 end
@@ -48,26 +48,28 @@ function node-install
 
   set -l arch (uname -sm)
   set -l filename "node-$_version-$_node_current_platform.tar.gz"
-  set -l tarball "$root/tarballs/$filename"
-  set -l target "$root/versions/node-$_version-$_node_current_platform/"
-  set -l shasumText "$root/checksums/$_version-SHASUMS256.txt"
+  set -l tarball "$FISH_NODE_ROOT/tarballs/$filename"
+  set -l target "$FISH_NODE_ROOT/versions/node-$_version-$_node_current_platform/"
+  set -l shasumText "$FISH_NODE_ROOT/checksums/$_version-SHASUMS256.txt"
 
   if not test -e $tarball
-    curl --fail --progress "$remote/$_version/$filename" > "$tarball"
+    echo "$FISH_NODE_REMOTE/$_version/$filename"
+    curl --fail --progress "$FISH_NODE_REMOTE/$_version/$filename" > "$tarball"
   end
 
   echo-success "downloaded"
 
   if not test -e $shasumText
-    curl --fail "$remote/$_version/SHASUMS256.txt" > "$shasumText"
+    echo "$FISH_NODE_REMOTE/$_version/SHASUMS256.txt"
+    curl --fail "$FISH_NODE_REMOTE/$_version/SHASUMS256.txt" > "$shasumText"
   end
 
-  fish -c "cd $root/tarballs/; and cat $shasumText | grep $filename | shasum -c - > /dev/null"
+  fish -c "cd $FISH_NODE_ROOT/tarballs/; and cat $shasumText | grep $filename | shasum -c - > /dev/null"
 
   echo-success "verified"
 
   if not test -e $target
-    tar -C "$root/versions"/ -zxf "$root/tarballs/$filename"
+    tar -C "$FISH_NODE_ROOT/versions"/ -zxf "$FISH_NODE_ROOT/tarballs/$filename"
   end
 
   echo-success "installed"
@@ -96,8 +98,8 @@ function node-set
   set -l _version (node-version-match $input_version)
   set -l filename "node-$_version-$_node_current_platform/bin"
 
-  if test -e "$root/versions/$filename"
-    set -gx PATH "$root/versions/$filename" $PATH
+  if test -e "$FISH_NODE_ROOT/versions/$filename"
+    set -gx PATH "$FISH_NODE_ROOT/versions/$filename" $PATH
     echo-success "node.current = $_version"
   else
     echo-failure  "node.current = $_version; not installed"
@@ -108,12 +110,12 @@ function node-set-global
   set -l input_version $argv[1]
   set -l _version (node-version-match $input_version)
   set -l filename "node-$_version-$_node_current_platform/bin"
-  set -l target  "$root/default/bin"
+  set -l target  "$FISH_NODE_ROOT/default/bin"
 
-  if test -e "$root/versions/$filename"
+  if test -e "$FISH_NODE_ROOT/versions/$filename"
     rm -rf $target
-    ln -s "$root/versions/$filename" $target
-    set -gx PATH "$root/versions/$filename" $PATH
+    ln -s "$FISH_NODE_ROOT/versions/$filename" $target
+    set -gx PATH "$FISH_NODE_ROOT/versions/$filename" $PATH
 
     echo-success "node.global = $_version"
   else
@@ -123,7 +125,7 @@ end
 
 function node-ls
   set -l _version "$argv[1]"
-  for node in (ls "$root/versions" | grep $_version)
+  for node in (ls "$FISH_NODE_ROOT/versions" | grep $_version)
     echo $node | cut -d '-' -f 2
   end
 end
@@ -134,19 +136,19 @@ function node-version-match
 end
 
 function node-ls-remote-refresh
-  rm $root/cache/versions.json
+  rm $FISH_NODE_ROOT/cache/versions.json
   node-ls-remote
 end
 
 function node-ls-remote
   set -l _version "$argv[1]"
-  if not test -e $root/cache/versions.json
-    curl https://nodejs.org/download/release/index.json 2> /dev/null > $root/cache/versions.json
+  if not test -e $FISH_NODE_ROOT/cache/versions.json
+    curl https://nodejs.org/download/release/index.json 2> /dev/null > $FISH_NODE_ROOT/cache/versions.json
   else
 
   end
 
-  cat $root/cache/versions.json | jq -r '.[].version' | grep $_version
+  cat $FISH_NODE_ROOT/cache/versions.json | jq -r '.[].version' | grep $_version
 end
 
 node-setup
