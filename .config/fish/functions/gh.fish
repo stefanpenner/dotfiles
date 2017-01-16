dd3a0f780e0a9b6b7ddc994f12b3b8aa5f400e1b
Stefan Penner <stefan.penner@gmail.com>
2017-01-15 16:07:14 -0800

    more cleanup

--- .config/fish/functions/gh.fish
+++ .config/fish/functions/gh.fish
@@ -1,12 +1,1 @@
-function gh -a fullname
-  set name (string split '/' $fullname)
-  set org $name[1]
-  set repo $name[2]
-  set git git@github.com:$org/$repo
-  set dest $HOME/src/$org/$repo
-  mkdir -p $HOME/src/$org 2> /dev/null
-  echo "$git -> $dest"
-  git clone $git $dest
-  cd $dest
-end
-
+/Users/spenner/.config/fisherman/gh/functions/gh.fish
