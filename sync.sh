6892a575570bfe3ce809a147b66e1989850ab396
Stefan Penner <stefan.penner@gmail.com>
2017-11-14 17:23:36 -0800

    fix up sync script

--- sync.sh
+++ sync.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 basename="$HOME/src/stefanpenner/dotfiles"
+backup=~/.dotfiles_backup/$(date +%m%d%H%M%Y%S)
 
 for file in $(find $basename -maxdepth 1 -iname '.*' -not -path '*/.git')
 do
-  mv ~/$file $backup 2>/dev/null
-  echo " $basename/$file"
-  ln -s $basename/$file ~
+  filename=$(basename $file)
+  mv "$HOME/$filename" "$backup" 2>/dev/null
+  rm -rf "$HOME/$filename"
+  echo " $file => ~/$filename"
+  ln -s "$file" ~
 done
